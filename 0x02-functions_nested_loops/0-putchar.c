3a0f50ebe007b974d3cc859e3b9e36d32c38dd3b
Perry Timinah <timnahwekalao@gmail.com>
2023-10-12 00:49:57 -0700

    betty coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,26 @@
 /*
  * File: 0-putchar.c
  * Auth: Perry T
- *
  */
 
 #include "main.h"
 
 /**
- * main - Prints "putchar" followed by a new line.
+ * main - Prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-   return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
